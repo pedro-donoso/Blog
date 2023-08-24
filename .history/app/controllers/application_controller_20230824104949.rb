2f9c0670f6443aa82b04d3d1788d06e12aeea73b
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-24 10:53:24 -0400

    login requerido al cerrar sesion

--- .history/app/controllers/application_controller_20230824104949.rb
+++ .history/app/controllers/application_controller_20230824104949.rb
@@ -4,8 +4,4 @@ class ApplicationController < ActionController::Base
         posts_path
     end
 
-    def after_sign_out_path_for(resource_or_scope)
-       new_user_session_path
-    end
-
 end
--- .history/app/controllers/application_controller_20230824105134.rb
+++ .history/app/controllers/application_controller_20230824105134.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_out_path_for(resource_or_scope)
-       new_user_session_path
+        root_path
     end
 
 end
